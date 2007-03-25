224ef044bae024fdc4f3976ca2ea150fdd270c20
rousseau <rousseau@0ce88b0d-b2fd-0310-8134-9614164e65ea>
2007-03-25 12:55:40 +0000

    licence is BSD, not GPL
    git-svn-id: svn://svn.debian.org/svn/pcsclite/trunk@2493 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- pycsc/setup.py
+++ pycsc/setup.py
@@ -11,7 +11,7 @@ description = '''Python class to communicate with smart cards'''
 
 classifiers = ["Development Status :: 5 - Production/Stable",
 "Intended Audience :: Developers",
-"License :: OSI Approved :: GNU General Public License (GPL)",
+"License :: OSI Approved :: BSD",
 "Natural Language :: English",
 "Operating System :: Microsoft :: Windows",
 "Operating System :: OS Independent",
